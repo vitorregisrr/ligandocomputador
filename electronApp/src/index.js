aaa1309ac94fb02f96e32b68d537bbb6bfe70c38
Vitor Régis <vitorregisrr@gmail.com>
2018-12-12 15:31:02 -0200

    electron loading gif

--- electronApp/src/index.js
+++ electronApp/src/index.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow } = require('electron');
-
+const path = require('path');
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -14,6 +14,8 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets/icon.png'),
+    loadingGif: path.join(__dirname, 'assets/icon.png')
   });
 
 
